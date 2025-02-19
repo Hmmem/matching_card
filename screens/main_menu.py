03b1e04477df359a64025a4640e85e2690c357fd
6710110276@psu.ac.th <6710110276@psu.ac.th>
2025-02-19 23:56:42 +0700

    Add: custom button class

--- screens/main_menu.py
+++ screens/main_menu.py
@@ -1,16 +1,39 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Color, RoundedRectangle
+
+
+class CustomButton(Button):
+    def __init__(self, **params):
+        super().__init__(**params)
+        self.font_size = 24
+        self.size_hint = (None, None)
+        self.size = (180, 70)
+        self.bold = True
+        self.color = (1, 1, 1, 1)
+        self.background_normal = ""
+        self.background_color = (0, 0, 0, 0)
+        self.border = (16, 16, 16, 16)
+
+        with self.canvas.before:
+            Color(0, 0.6, 1, 1)
+            self.rect = RoundedRectangle(size=self.size, pos=self.pos, radius=[20])
+
+        self.bind(pos=self.update_rect, size=self.update_rect)
+
+    def update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
 
 
 class Mainmenuscreen(Screen):
     def __init__(self, **params):
         super().__init__(**params)
-        layout = BoxLayout(orientation="vertical")
+        layout = FloatLayout()
 
-        start_button = Button(
+        start_button = CustomButton(
             text="Start Game",
-            size_hint=(0.3, 0.2),
             pos_hint={"center_x": 0.5, "center_y": 0.5},
         )
         start_button.bind(on_press=self.switch_to_game)
