594fe44c1509962acbc48972fe9804afd0dc8820
Jongkoch Siriphet <jongkoch.siriphet@gmail.com>
2025-02-24 13:26:38 +0700

    back to menu success

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from kivy.uix.screenmanager import ScreenManager
 from screens import Mainmenuscreen, Gamescreen
 from screens.difficulty_screen import DifficultyScreen
 
-
+class MyScreenManager(ScreenManager):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_widget(Mainmenuscreen(name="main_menu"))
+        self.add_widget(Gamescreen(name="game_screen"))
 
 class MyGame(App):
     def build(self):
         SM = ScreenManager()
-        SM.add_widget(Mainmenuscreen(name="Main"))
+        SM.add_widget(Mainmenuscreen(name="main_menu"))
         SM.add_widget(DifficultyScreen(name="Difficulty"))
         SM.add_widget(Gamescreen(name="Game"))
         return SM
