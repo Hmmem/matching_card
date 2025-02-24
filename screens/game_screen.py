c8bd7032b5d328a78c4519f64103fc22bd437b6a
Jongkoch Siriphet <jongkoch.siriphet@gmail.com>
2025-02-24 13:16:23 +0700

    add back to menu button

--- screens/game_screen.py
+++ screens/game_screen.py
@@ -2,15 +2,32 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
 from components import CardManager
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 Builder.load_file("gamesceenstyle.kv")
 
 class Gamescreen(Screen):
     def __init__(self, **params):
         super().__init__(**params)
+        main_layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
         self.grid = GridLayout(cols=4, spacing=10, padding=20, size_hint=(None, None), pos_hint={"center_x": 0.5, "center_y": 0.65})
         self.card_manager = CardManager(self.grid)
         self.add_widget(self.grid)
+
+        back_button = Button (
+            text="Back to Menu",
+            size_hint=(None, None),
+            size=(200, 50),
+            pos_hint={"center_x":0.5}
+        )
+        back_button.bind(on_release=self.go_to_menu)
+        main_layout.add_widget(back_button)
+
+        self.add_widget(main_layout)
+
+    def go_to_menu(self, instance):
+        self.manager.current = "main_menu"  # เปลี่ยนกลับไปที่เมนูหลัก
 
     def set_difficulty(self, difficulty):
         self.card_manager.set_difficulty(difficulty)
